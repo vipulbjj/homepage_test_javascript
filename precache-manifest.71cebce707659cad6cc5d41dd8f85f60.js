a0185b6182e0213869862771f7c3e81804af8984
Vipul Bajaj <vipulbjj@iitk.ac.in>
2020-12-20 02:57:55 +0530

    pushed new template

---
+++ precache-manifest.71cebce707659cad6cc5d41dd8f85f60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7778a4c3e6171b5c5c1e3bd9abf91dd",
+    "url": "/homepage/index.html"
+  },
+  {
+    "revision": "674fbd7295c65f0a1134",
+    "url": "/homepage/static/css/2.a641e29a.chunk.css"
+  },
+  {
+    "revision": "c6d93805131c532d82c9",
+    "url": "/homepage/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "674fbd7295c65f0a1134",
+    "url": "/homepage/static/js/2.9a7b0f77.chunk.js"
+  },
+  {
+    "revision": "c6d93805131c532d82c9",
+    "url": "/homepage/static/js/main.63f3a708.chunk.js"
+  },
+  {
+    "revision": "9f75ca5555434e3c08ed",
+    "url": "/homepage/static/js/runtime-main.b1add913.js"
+  }
+]);
